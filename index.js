a1c064ab9259ae34208111ec2018e7e0c6f75073
alitohidi <allitohidi99@gmail.com>
2023-05-29 05:20:00 +0330

    index.js modified

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ console.log("hello git...");
 
 // function
 console.log("hi ...");
-
+console.log("ali...");
+console.log("ali...");
+console.log("ali...");
+console.log("ali...");
+console.log("ali...");
 // fun
